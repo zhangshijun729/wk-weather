a88a37f4b534cabec99a6b72f153f42b0a408bb5
JRG-zhangshijun <462895506@qq.com>
2017-12-12 19:47:35 +0800

    不能获取城市id

---
+++ app.js
@@ -0,0 +1,18 @@
+App({
+    onLaunch() {
+        console.log('launch')
+    },
+    onShow(opth) {
+        console.log('show')
+    },
+    onHide() {
+        console.log('hide')
+    },
+    onError() {
+        console.log('error')
+    },
+    globalData: {
+        weather: null,
+        hourly: null
+    }
+})
